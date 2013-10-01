c7c139ea1c9f108a1784786aa976234575fa7413
woofgl <woofgl@gmail.com>
2013-10-01 14:24:19 +0800

    change site to project name

--- create.sh
+++ create.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 curDir="$(pwd)"
 templateDir="$(dirname $(readlink -f $0))"
-projectDir=$1
+projectName=$1
+projectDir=$1_project
 
 # echo $(basename $templateDir)
 # echo $curDir
@@ -16,18 +17,20 @@ rm -f template.zip
 # zip -q -r  template.zip ../$(basename $templateDir)/* 
 zip -x create.sh  -q -r template.zip ./* 
 cd $curDir
-# mkdir $projectDir
+mkdir $projectDir
  
-django-admin.py startproject --template=$templateDir/template.zip --extension=py,rst,html $projectDir
-cd $curDir
-mkvirtualenv -a $projectDir $projectDir"-dev"
+django-admin.py startproject --template=$templateDir/template.zip --extension=py,rst,html $projectName $projectDir
+cd $projectDir
+mkvirtualenv -a `pwd` $projectName"-dev"
 # cd $projectDir
+touch .gitignore
+echo .idea >>.gitignore
  # mkvirtualenv $1"_dev"
 
  # virEnv=$(cygpath -v $(pwd))
   # virEnv=$(basename $(pwd))
  # echo $virEnv
  # add2virtualenv $virEnv
-git flow init
+git flow init -d
 # workon $1"_dev"
 
--- project_name/project_name/settings/base.py
+++ project_name/project_name/settings/base.py
@@ -45,12 +45,13 @@ MANAGERS = ADMINS
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db/development.sqlite3', # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '', # Set to empty string for default.
     }
 }
 ########## END DATABASE CONFIGURATION
