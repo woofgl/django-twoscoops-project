583daf722bb6b715aad39003fe13f4ee7e6b50d4
woofgl <woofgl@gmail.com>
2013-10-03 23:22:47 +0800

    add python autocomplete config support

--- create.sh
+++ create.sh
@@ -21,16 +21,64 @@ mkdir $projectDir
  
 django-admin.py startproject --template=$templateDir/template.zip --extension=py,rst,html $projectName $projectDir
 cd $projectDir
-mkvirtualenv -a `pwd` $projectName"-dev"
+hintDir=`pwd`
+mkvirtualenv -a `pwd`/$projectName $projectName"-dev"
+cd ..
 # cd $projectDir
 touch .gitignore
 echo .idea >>.gitignore
+echo .codeintel >>.gitignore
+echo *.pyc >>.gitignore
  # mkvirtualenv $1"_dev"
 
  # virEnv=$(cygpath -v $(pwd))
   # virEnv=$(basename $(pwd))
  # echo $virEnv
  # add2virtualenv $virEnv
+# init git flow
 git flow init -d
+pip install -r requirements/local.txt
+
+# mkdir .codeintel
+# cd .codeintel
+# touch config
+# # create config file
+
+# echo  { >> config
+# echo -e '\t' '"Python"': { >>config
+# echo -e '\t\t'    \"python\": \"c:/Users/Administrator/.virtualenvs/$projectName-dev/Scripts/python\", >> config 
+# echo -e '\t\t'     \"pythonExtraPaths\": [ >> config
+# echo -e '\t\t\t\t'       \"c:/Users/Administrator/.virtualenvs/$projectName-dev/Lib/site-packages\", >>config 
+# echo -e '\t\t\t\t'           \"`cygpath -v $hintDir`\" >> config
+# echo -e '\t\t\t'        ] >> config 
+# echo -e '\t'    } >> config
+# echo } >> config
+
+# # create sublime project
+# cd ..
+subProject=$projectName.sublime-project
+touch $subProject
+echo { >> $subProject
+echo -e '\t' \"folders\": >> $subProject
+echo -e '\t\t'   [ >> $subProject
+echo -e '\t\t\t'        { >> $subProject
+echo -e '\t\t\t\t'     \"follow_symlinks\": true, >> $subProject
+echo -e '\t\t\t\t'     \"path\": \"$(cygpath -v `pwd`)\" >> $subProject
+echo -e '\t\t\t'        } >> $subProject
+echo -e '\t\t'    ] ,>> $subProject
+
+echo -e '\t'  \"settings\": { >> $subProject
+echo -e '\t\t'    \"python\": \"c:/Users/Administrator/.virtualenvs/$projectName-dev/Scripts/python\", >> $subProject
+echo -e '\t\t'   \"extra_paths\": [  >> $subProject
+echo -e '\t\t\t'       \"c:/Users/Administrator/.virtualenvs/$projectName-dev/Lib/site-packages\", >> $subProject 
+echo -e '\t\t\t'           \"`cygpath -v $hintDir`\" >> $subProject
+echo -e '\t\t'       ]  >> $subProject
+echo -e '\t\t'   }  >> $subProject
+
+echo -e '\t' } >> $subProject
+
+# open project
+exec d:/tools/sublime_text/sublime_text $subProject &
+
 # workon $1"_dev"
 
